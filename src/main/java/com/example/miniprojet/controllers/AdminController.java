72a286ce4bf3026fcc7157f72b726b4ef9aa97c2
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2022-05-13 12:35:11 +0300

    config

---
+++ src/main/java/com/example/miniprojet/controllers/AdminController.java
@@ -0,0 +1,17 @@
+package com.example.miniprojet.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+
+public class AdminController {
+    @RequestMapping("/")
+    public String index(HttpServletRequest request){
+        return "index";
+    }
+}
